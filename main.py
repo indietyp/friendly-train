74b9d3094d51fe6c0ebfec1a7048938334845a7d
indietyp <bilalmahmoud@posteo.de>
2017-03-05 12:53:09 +0100

    this path is hot as fuck

--- main.py
+++ main.py
@@ -1,46 +1,99 @@
 import json
 import yaml
+import math
 import os
 import re
-import uuid
 import string
 import random
 from copy import deepcopy
 
 
 class LanguageGenerator3000:
-  def __init__(self):
-    pass
+  def __init__(self, configpath='config.yaml', filepath='main.json', sample_size=500):
+    print('started')
+    filetype = configpath.split('.')[-1]
+    file = open(configpath, 'r').read()
 
-  def generate_single_sentence(self, *args, **kwargs):
-    return {'sentence': 'oggle gobble dingdong!',
-            'raw': {
-                'text': [
-                    {'type': 'subject', 'word': 'oggle'},
-                    {'type': 'subject', 'word': 'gobble'},
-                    {'type': 'object', 'word': 'dingdong'},
-                ],
-                'punctuation': '!'
-            }
-            }
+    if type(file) == bytes:
+      file = file.decode()
 
-  def generate_sentence_list(self, sample_size=3, *args, **kwargs):
-    return [{'sentence': 'oggle gobble dingdong!',
-            'raw': {
-                'text': [
-                    {'type': 'subject', 'word': 'oggle'},
-                    {'type': 'subject', 'word': 'gobble'},
-                    {'type': 'object', 'word': 'dingdong'},
-                ],
-                'punctuation': '!'
-            }
-    },
-    ]
+    if filetype == 'yaml':
+      rules = yaml.load(file)
+    elif filetype == 'json':
+      rules = json.loads(file)
+
+    converted, _, _ = self.convert(rules)
+    self.generate_word_list(sample_size, False, filepath)
+    # self.generate_word_list(sample_size)
+
+    print('Generating probabilities...')
+    self.generate_probabilities()
+
+  def generate_single_sentence(self, sequences, punctuation, weighted=True):
+    output = {'raw': {'text': []}, 'sentence': ''}
+    last = ''
+    for sequence in sequences:
+      if output['sentence'] == '' or not weighted:
+        words = [word for word in self.wordlist if word['type'] == sequence]
+        selected = random.choice(words)
+      else:
+        selected = self.get_correlated_word_str(last, sequence)
+        selected = {'word': selected, 'type': deepcopy(sequence)}
+
+      output['raw']['text'].append(selected)
+      output['sentence'] += selected['word'] + ' '
+      last = deepcopy(selected)
+
+    output['sentence'] = output['sentence'][:-1] + punctuation
+    return output
+
+  def generate_sentence_list(self, sample_size=3, weighted=True):
+    rules = deepcopy(self.rules)
+    sample_size = int(sample_size)
+
+    no_probability = []
+    selector = ''
+    used = 0
+
+    for pointer in range(len(rules['sentence structure'])):
+      if rules['sentence structure'][pointer]['probability'] == -1:
+        no_probability.append(pointer)
+      else:
+        used += rules['sentence structure'][pointer]['probability']
+        selector += str(pointer) * int(rules['sentence structure'][pointer]['probability'] * sample_size)
+        print('prob' + str(int(rules['sentence structure'][pointer]['probability'] * sample_size)))
+
+    if used < sample_size:
+      for pointer in no_probability:
+        selector += '{}'.format(pointer) * int(((1 - used) / (len(no_probability))) * sample_size)
+
+    output = []
+    for _ in range(sample_size):
+      selected = rules['sentence structure'][int(random.choice(selector))]
+
+      sequence = []
+      for wordtype in selected['letters'][:-1]:
+        actual = ''
+        for key, item in rules['words'].items():
+          if isinstance(item, list):
+            if ''.join(item[0]['letters']) == wordtype:
+              actual = key
+          elif isinstance(item, dict) and 'identification' in item.keys():
+            if ''.join(item['identification'][0]['letters']) == wordtype:
+              actual = key
+          else:
+            raise ValueError('wow! You did it! Amazing! You tripped the only error Error in the whole class')
+        if actual != '':
+          sequence.append(actual)
+
+      output.append(self.generate_single_sentence(sequence, selected['letters'][-1], weighted))
+
+    return output
 
   def generate_single_word(self, word_type):
     rules = deepcopy(self.rules)
 
-    if isinstance(rules['words'][word_type], dict) and 'extends' in rules['words'][word_type]:
+    if isinstance(rules['words'][word_type], dict) and 'extends' in rules['words'][word_type] and 'rules' in rules['words'][word_type]['extends']:
       rules['words'][word_type]['extends']['rules'] = self.__vowel_consonant_replacer(rules['words'][word_type]['extends']['rules'], self.vowels, self.consonants)
       for key, items in rules['words'][word_type]['extends'].items():
         if key == rules:
@@ -78,22 +131,21 @@ class LanguageGenerator3000:
         letters = random.choice(random.choice(stuff))
       else:
         letters = ''
-        print(applied_rule[0])
         applied_rule = applied_rule[0]
 
         selector = ''
-        no_probablity = []
+        no_probability = []
         used = 0
         for pointer in range(0, len(applied_rule)):
-          if applied_rule[pointer]['probablity'] == -1:
-            no_probablity.append(pointer)
+          if applied_rule[pointer]['probability'] == -1:
+            no_probability.append(pointer)
 
-          used += applied_rule[pointer]['probablity']
-          selector += str(pointer) * int(applied_rule[pointer]['probablity'] * 100)
+          used += applied_rule[pointer]['probability']
+          selector += str(pointer) * int(applied_rule[pointer]['probability'] * 100)
 
         if used < 1:
-          for pointer in no_probablity:
-            selector += '{}'.format(pointer) * int(((1 - used) / len(no_probablity)) * 100)
+          for pointer in no_probability:
+            selector += '{}'.format(pointer) * int(((1 - used) / len(no_probability)) * 100)
 
         selected = int(random.choice(selector))
 
@@ -116,11 +168,11 @@ class LanguageGenerator3000:
 
     return {'type': word_type, 'word': word}
 
-  def generate_word_list(self, sample_size=50, sort=False):
+  def generate_word_list(self, sample_size=50, sort=False, filepath='main.json'):
     rules = deepcopy(self.rules)
 
-    if os.path.isfile('wordlist.json'):
-      with open('wordlist.json', 'r') as out:
+    if os.path.isfile(filepath):
+      with open(filepath, 'r') as out:
         wordlist = json.loads(out.read())
         if sort:
           wordlist = sorted(wordlist, key=lambda x: x['word'])
@@ -145,7 +197,7 @@ class LanguageGenerator3000:
           for _ in range(int((sample_size - generated) / len(no_limit))):
             wordlist.append(self.generate_single_word(key))
 
-      with open('wordlist.json', 'w') as out:
+      with open(filepath, 'w') as out:
         out.write(json.dumps(wordlist))
 
       if sort:
@@ -162,11 +214,20 @@ class LanguageGenerator3000:
 
   def get_correlated_word_str(self, word, word_type):
     if isinstance(word, str):
-      wordstr = word
+      # wordstr = word
+      raise ValueError('not permitted')
     else:
-      wordstr = word['word']
-    correlated_words_dict = self.__weights[word_type][wordstr]
-    random_number = random.random()
+      pass
+
+    words_by_type = [x['word'] for x in self.wordlist if x['type'] == word_type]
+    correlated_words_dict = self.__weights[word['type']][word['word']]
+    correlated_words_dict = [{k: v} for k, v in correlated_words_dict.items() if k in words_by_type]
+
+    tmp = {}
+    [tmp.update(x) for x in correlated_words_dict]
+    correlated_words_dict = deepcopy(tmp)
+
+    random_number = random.random() * sum(correlated_words_dict.values())
     for correlated_word in correlated_words_dict:
       random_number -= correlated_words_dict[correlated_word]
       if random_number <= 0:
@@ -174,21 +235,32 @@ class LanguageGenerator3000:
 
   def generate_probabilities(self, randomness=0.2):
     # generate giant sentence list
-    sentence_basis = self.generate_sentence_list(count=(1 / randomness) * self.wordcount(), weighted=False)
+    rules = deepcopy(self.rules)
+    sentence_basis = self.generate_sentence_list((1 / randomness) * self.wordcount(), False)
     occurances_by_word_type = {}
+
+    flatten = {}
+    for item in self.wordlist:
+      flatten[item['word']] = 0.0
+
+    for key, item in rules['words'].items():
+      occurances_by_word_type[key] = {}
+      for word in [word for word in self.wordlist if word['type'] == key]:
+        occurances_by_word_type[key][word['word']] = deepcopy(flatten)
+
     # count occurances
     for single_sentence in sentence_basis:
       for single_word in single_sentence['raw']['text']:
         word_type = single_word['type']
-        if word_type not in occurances_by_word_type:
-          occurances_by_word_type[word_type] = {}
-        if self.get_word_str(single_word) not in occurances_by_word_type[word_type]:
-          occurances_by_word_type[word_type][self.get_word_str(single_word)] = {}
+        # if word_type not in occurances_by_word_type:
+        #   occurances_by_word_type[word_type] = {}
+        # if self.get_word_str(single_word) not in occurances_by_word_type[word_type]:
+        #   occurances_by_word_type[word_type][self.get_word_str(single_word)] = {}
 
         for correlated_word in single_sentence['raw']['text']:
+          # if self.get_word_str(correlated_word) not in occurances_by_word_type[word_type][self.get_word_str(single_word)]:
+          #   occurances_by_word_type[word_type][self.get_word_str(single_word)] = {}
           if self.get_word_str(correlated_word) not in occurances_by_word_type[word_type][self.get_word_str(single_word)]:
-            occurances_by_word_type[word_type][self.get_word_str(single_word)][self.get_word_str(correlated_word)] = {}
-          if self.get_word_str(correlated_word) not in occurances_by_word_type[word_type][self.get_word_str(single_word)][self.get_word_str(correlated_word)]:
             occurances_by_word_type[word_type][self.get_word_str(single_word)][self.get_word_str(correlated_word)] = 0
 
           occurances_by_word_type[word_type][self.get_word_str(single_word)][self.get_word_str(correlated_word)] += 1
@@ -232,16 +304,20 @@ class LanguageGenerator3000:
         for correlated_word in occurances_by_word_type[word_type][single_word]:
           occurances_by_word_type[word_type][single_word][correlated_word] *= all_weights_by_word[correlated_word]
 
-    # normalize (sum of probabilities to 1)
+    # # normalize (sum of probabilities to 1)
+    word_count = self.wordcount()
     for word_type in occurances_by_word_type:
       for single_word in occurances_by_word_type[word_type]:
         sum_of_occurances = 0
         # 1. count occurances
-        for correlated_word in occurances_by_word_type[word_type][single_word]:
-          sum_of_occurances += occurances_by_word_type[word_type][single_word][correlated_word]
+        # for correlated_word in occurances_by_word_type[word_type][single_word]:
+        # sum_of_occurances += occurances_by_word_type[word_type][single_word][correlated_word]
         # 2. normalize
         for correlated_word in occurances_by_word_type[word_type][single_word]:
-          occurances_by_word_type[word_type][single_word][correlated_word] /= sum_of_occurances
+          # sum_of_occurances
+          occurances_by_word_type[word_type][single_word][correlated_word] = math.log(occurances_by_word_type[word_type][single_word][correlated_word] * (word_count / 10), 3) / 5
+        # 3. set same word to zero
+        occurances_by_word_type[word_type][single_word][single_word] = 0.0
 
     # save result
     self.__weights = occurances_by_word_type
@@ -252,11 +328,11 @@ class LanguageGenerator3000:
     end_consonant = ''
 
     used = 0
-    no_probablity = []
+    no_probability = []
     for charlist in converted['vowels']['charlist']:
       if 'INHERIT' in charlist['letters']:
-          # if alphabet['probablity'] != -1:
-          #   no_probablity.append(alphabet[])
+          # if alphabet['probability'] != -1:
+          #   no_probability.append(alphabet[])
 
         for alphabet in converted['alphabet']:
           vowels = ''
@@ -274,40 +350,45 @@ class LanguageGenerator3000:
           consonant = re.findall(r'([^{}])'.format(','.join(list(vowels))), ''.join(alphabet['letters']))
           consonant = ''.join(consonant)
 
-          if alphabet['probablity'] == -1:
-            no_probablity.append(vowels)
+          if alphabet['probability'] == -1:
+            no_probability.append(vowels)
           else:
-            used += alphabet['probablity']
-            end_vowels += vowels * int(alphabet['probablity'] * 100)
-            end_consonant += consonant * int(alphabet['probablity'] * 100)
+            used += alphabet['probability']
+            end_vowels += vowels * int(alphabet['probability'] * 100)
+            end_consonant += consonant * int(alphabet['probability'] * 100)
 
       else:
-        end_vowels += ''.join(charlist['letters'] * int(charlist['probablity'] * 100))
-        used += charlist['probablity']
+        if charlist['probability'] != -1:
+          end_vowels += ''.join(charlist['letters'] * int(charlist['probability'] * 100))
+          used += charlist['probability']
+        else:
+          no_probability.append(''.join(charlist['letters']))
 
-    if used < 1 and used > 0:
-      for vowels in no_probablity:
-        end_vowels += vowels * int(((1 - used) / len(no_probablity)) * 100)
-        end_consonant += consonant * int(alphabet['probablity'] * 100)
+    if used < 1:
+      for vowels in no_probability:
+        # letters = ''.join([''.join(x['letters']) for x in converted['alphabet']])
+        # consonant = re.findall(r'([^{}])'.format(','.join(list(vowels))), ''.join(letters))
+        # consonant = ''.join(consonant)
+
+        end_vowels += vowels * int(((1 - used) / len(no_probability)) * 100)
+        # end_consonant += consonant * int(((1 - used) / len(no_probability)))
 
     vowels = end_vowels
     consonant = end_consonant
-
     if consonant == '':
       letters = []
-      no_probablity = []
+      no_probability = []
       used = 0
       for alphabet in converted['alphabet']:
-        print(alphabet)
-        if alphabet['probablity'] == -1:
-          no_probablity.append(alphabet['letters'])
+        if alphabet['probability'] == -1:
+          no_probability.append(alphabet['letters'])
         else:
-          used += alphabet['probablity']
-          letters += alphabet['letters'] * int(alphabet['probablity'] * 100)
+          used += alphabet['probability']
+          letters += alphabet['letters'] * int(alphabet['probability'] * 100)
 
       if used < 1:
-        for tmp in no_probablity:
-          vowels += ''.join(tmp * int(((1 - used) / len(no_probablity)) * 100))
+        for tmp in no_probability:
+          letters += ''.join(tmp * int(((1 - used) / len(no_probability)) * 100))
 
       consonant = re.findall(r'([^{}])'.format(','.join(list(set(vowels)))), ''.join(letters))
       consonant = ''.join(consonant)
@@ -372,7 +453,7 @@ class LanguageGenerator3000:
 
           item = list(search[0])
           new = {}
-          new['probablity'] = -1 if len(item) < 2 else float(item[1][1:-1])
+          new['probability'] = -1 if len(item) < 2 else float(item[1][1:-1])
           if item[0] in ['GERMAN', 'ENGLISH', 'LATIN']:
             mode = item[0]
             item[0] = string.ascii_lowercase
@@ -404,25 +485,7 @@ class LanguageGenerator3000:
   def get_weights(self):
     return self.__weights
 
-  def generate(self, filepath="./config.yaml", sample_size=500):
-    print('started')
-    filetype = filepath.split('.')[-1]
-    file = open(filepath, 'r').read()
-
-    if type(file) == bytes:
-      file = file.decode()
-
-    if filetype == 'yaml':
-      rules = yaml.load(file)
-    elif filetype == 'json':
-      rules = json.loads(file)
-
-    converted, _, _ = self.convert(rules)
-    print(self.generate_word_list(500, sort=True))
-    self.generate_probabilities()
-
-    print('Generating probabilities...')
-
 
 if __name__ == '__main__':
-  LanguageGenerator3000().generate()
+  for _ in LanguageGenerator3000(sample_size=1000).generate_sentence_list(sample_size=10):
+    print(_['sentence'] + '\n')
